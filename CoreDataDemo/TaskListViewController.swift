d2987782ee2854b9d52f0b9f274913b3bd90f5db
Ruslan <ruslan.shtybaev@gmail.com>
2022-04-17 12:29:14 +0300

    add new method

--- CoreDataDemo/TaskListViewController.swift
+++ CoreDataDemo/TaskListViewController.swift
@@ -23,10 +23,24 @@ class TaskListViewController: UITableViewController {
         let navBarAppearance = UINavigationBarAppearance()
         
         navBarAppearance.backgroundColor = UIColor(red: 21/255, green: 101/255, blue: 194/255, alpha: 192/255)
+        
         navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
         navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+                                                     
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addNewTask)
+        )
+        navigationController?.navigationBar.tintColor = .white
+        
         navigationController?.navigationBar.standardAppearance = navBarAppearance
         navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+    }
+    
+    @objc private func addNewTask() {
+        
     }
 }
 
